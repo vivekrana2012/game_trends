8b205d28632d607b625dd6f15420281825988a8b
Vivek Rana <vivekrana.2012@gmail.com>
2018-04-03 21:39:11 +0530

    Added videos

--- static/controller.js
+++ static/controller.js
@@ -1,7 +1,16 @@
 var app = angular.module('game_trends', []);
-app.controller('AppController', function($scope, $http) {
-    $http.get('/dota2_youtube_rss', function(response){
-      $scope.dota2_videolist = response;
-      console.log($scope.dota2_videolist);
-    });
+app.controller('AppController', function($scope, $http, $sce) {
+  $http.get("/dota2_youtube_rss")
+  .then(function(response) {
+    $scope.dota2matches = response.data;
+
+    $scope.trustSrc = function(src) {
+      return $sce.trustAsResourceUrl(src);
+    }
+
+    for(var i = 0; i<$scope.dota2matches.length; i++){
+      $scope.dota2matches[i].id = "https://www.youtube.com/embed/".concat($scope.dota2matches[i].id);
+    }
+
+  });
 });
